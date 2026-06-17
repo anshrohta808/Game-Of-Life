22dacb388d120cd23bc40163ae4c6b6d47e70299
anshrohta808 <ansh.rohta808@gmail.com>
2026-06-17 16:53:14 +0530

    Game background color

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,11 @@
 #include <raylib.h>
 const int winWIDTH = 750;
 const int winHEIGHT = 750;
-
+Color background = {43, 45, 31, 1};
 int main(int argc, char const *argv[])
 {
    InitWindow(winWIDTH, winHEIGHT, "GAME SCREEN");
-   // setting the fps limit for the game (ceiling for max limit of frames per second)
+   // setting the fps limit for the game (ceiling for max limit of frames per secondme)
    SetTargetFPS(10);
    // Game Simulation Loop
 
@@ -17,6 +17,7 @@ int main(int argc, char const *argv[])
 
       // DRAWING GAME
       BeginDrawing();
+      ClearBackground(background);
       EndDrawing();
    }
 
